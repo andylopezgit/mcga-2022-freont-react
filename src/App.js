15ff73f040a79405e403502c0aab8b178df8b351
andylopezgit <alopezciotta@gmail.com>
2022-09-20 21:25:45 -0300

    clases martes 20/09

--- src/App.js
+++ src/App.js
@@ -1,39 +1,23 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Task } from "./components/task/task";
 import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
-import { Login } from "./components/login/login";
+import { Login } from "./screems/Login/login.js";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./components/home/home";
+import { Products } from "./screems/Products/products";
+import { Layout } from "./components/Layout/layout";
+
+// darle estilos al layout, vista de productos con una tabla, crear un componente compartido, agregarle footer al layout
 
 function App() {
-  const [task, setTask] = useState([]);
-
-  const addTask = (e) => {
-    setTask(e.target.value);
-  };
-  const addTask2 = (event) => {
-    event.preventDefault();
-    const id = uuidv4();
-    console.log(task);
-    console.log(id);
-  };
-
   return (
-    <div className="container">
-      <Login />
-      {/*<div className='title'>*/}
-      {/*    <h2>Task Maneger</h2>*/}
-      {/*</div>*/}
-
-      {/*<form onSubmit={addTask2}>*/}
-      {/*    <label>*/}
-      {/*        Name:*/}
-      {/*        <input type="text" value={task} onChange={addTask}/>*/}
-      {/*    </label>*/}
-      {/*    <input type="submit" value="Submit"/>*/}
-      {/*</form>*/}
-      {/*<Task task={task} completed="true"/>*/}
-    </div>
+    <Layout>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+    </Layout>
   );
 }
 
--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,37 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./login.css";
+import { Route, Router } from "react-router-dom";
 
 function Login() {
+  const [user, setUser] = React.useState([]);
+  const [pass, setPass] = React.useState([]);
 
-    const [user, setUser] = React.useState([]);
-    const [pass, setPass] = React.useState([])
+  useEffect(() => {
+    console.log(user);
+    if (user === "Jorge") return;
+    setUser("Jorge");
+  }, [user]);
 
-    const handleUser = (e) => {
-        setUser(e.target.value)
-    }
+  const handleUser = (e) => {
+    setUser(e.target.value);
+  };
 
-    const handlePass = (e) => {
-        setPass(e.target.value)
-    }
+  const handlePass = (e) => {
+    setPass(e.target.value);
+  };
 
-    const login = (e) => {
-        e.preventDefault()
-        console.log(user, pass)
+  const login = (e) => {
+    e.preventDefault();
+    console.log(user, pass);
+  };
 
-    }
-
-    return (
-        <section>
-            <form action="" className="formLogin" onSubmit={login}>
-                <label htmlFor="">User:</label>
-                <input type="text" onChange={handleUser}/>
-                <label htmlFor="">Pass:</label>
-                <input type="password" onChange={handlePass}/>
-                <input type="submit" value="Login" className="btnLogin"/>
-            </form>
-        </section>
-    )
-
+  return (
+    <section>
+      <form action="" className="formLogin" onSubmit={login}>
+        <label htmlFor="">User:</label>
+        <input type="text" onChange={handleUser} />
+        <label htmlFor="">Pass:</label>
+        <input type="password" onChange={handlePass} />
+        <input type="submit" value="Login" className="btnLogin" />
+      </form>
+    </section>
+  );
 }
 
-export {Login}
+export { Login };

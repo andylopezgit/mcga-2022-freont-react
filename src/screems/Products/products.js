23e31f3a6df4007fb933e691ffb9c924984f023e
andylopezgit <alopezciotta@gmail.com>
2022-09-27 08:55:55 -0300

    se agregar router navigate en login

--- src/screems/Products/products.js
+++ src/screems/Products/products.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Products = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        return response.json();
+      })
+      .then((datos) => {
+        setProducts(datos);
+      });
+  }, [products]);
   return (
     <div>
-      <h1>vista producto</h1>
+      <h1>Tabla de producto</h1>
+      {products.length}
+      {products.map((product) => {
+        return (
+          <table>
+            <tr>
+              <th>Id</th>
+              <th>Name</th>
+              <th>Price</th>
+            </tr>
+            <tr>
+              <td>{product.id}</td>
+              <td>{product.name}</td>
+              <td>{product.username}</td>
+            </tr>
+          </table>
+        );
+      })}
     </div>
   );
 };

16c72277a6f3bf971eacedd2585903a5e53f5bf5
andylopezgit <alopezciotta@gmail.com>
2022-10-18 14:43:22 -0300

    practica, se actualiza el estado pero no se ve en la vista de componente

--- src/screems/Practice/practice.js
+++ src/screems/Practice/practice.js
@@ -1,30 +1,26 @@
 import React from "react";
-import {useDispatch, useSelector} from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
-import {decreaseCounter, increaseCounter} from "../../redux/Product/actions";
-import {getProducts} from "../../redux/BackendProduct/actions";
+import { decreaseCounter, increaseCounter } from "../../redux/Product/actions";
+import { getProducts } from "../../redux/BackendProduct/actions";
 
 function Practice() {
-    const dispatch = useDispatch();
-    const {count} = useSelector((state) => state.counter);
-    const {product} = useSelector((state) => state.product);
-    return (
-        <>
-            <div>Count: {count}</div>
-
-            <button onClick={() => dispatch(increaseCounter())}>
-                Increase Count
-            </button>
-
-            <button onClick={() => dispatch(decreaseCounter())}>
-                Decrease Count
-            </button>
-
-            <p>Post: {JSON.stringify(product)}</p>
-
-            <button onClick={() => dispatch(getProducts())}>Traer</button>
-        </>
-    );
+  const dispatch = useDispatch();
+  const { count } = useSelector((state) => state.counter);
+  const { product } = useSelector((state) => state.product);
+  return (
+    <>
+      <div>Count: {count}</div>
+      <button onClick={() => dispatch(increaseCounter())}>
+        Increase Count
+      </button>
+      <button onClick={() => dispatch(decreaseCounter())}>
+        Decrease Count
+      </button>
+      <p>Product: {JSON.stringify(product)}</p>
+      <button onClick={() => dispatch(getProducts())}>Traer</button>
+    </>
+  );
 }
 
 export default Practice;
